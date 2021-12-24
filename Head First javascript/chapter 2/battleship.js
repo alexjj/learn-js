6f18837bfa469f9912123b9efd06017e2eb3bcd3
Alex Johnstone <alexjj@gmail.com>
2021-12-24 12:59:09 +0000

    battleship

--- Head First javascript/chapter 2/battleship.js
+++ Head First javascript/chapter 2/battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 var guess;
 var hits = 0;
@@ -14,7 +15,20 @@ while (isSunk == false) {
         alert("Please enter a number between 0-6!");
     } else {
         guesses = guesses + 1
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank by battleship!");
+            }
+        } else {
+            alert("MISS!");
+        }
     }
 }
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
 
+alert(stats);
 /* page 55 */
